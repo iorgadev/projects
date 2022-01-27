b221646b4ceba4fa50273da04cf9370a723c99d6
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-01-27 11:42:52 -0500

    changes to perspective ui

--- pages/projects/wip/perspective.tsx
+++ pages/projects/wip/perspective.tsx
@@ -1,8 +1,48 @@
-import React, { useRef, Suspense } from "react";
+import React, { useRef, Suspense, useState, useEffect } from "react";
 import * as THREE from "three";
 import useMouse from "@react-hook/mouse-position";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
+
+export function CustomCursor() {
+  const cursorRef = useRef(null);
+  useEffect(() => {
+    if (cursorRef.current == null || cursorRef == null) return;
+    document.addEventListener("mousemove", (e) => {
+      if (cursorRef.current == null) return;
+      cursorRef.current.setAttribute(
+        "style",
+        "top: " + e.pageY + "px; left: " + e.pageX + "px;"
+      );
+    });
+    document.addEventListener("click", () => {
+      if (cursorRef.current == null) return;
+      cursorRef.current.classList.add("expand");
+      setTimeout(() => {
+        if (cursorRef.current == null) return;
+        cursorRef.current.classList.remove("expand");
+      }, 500);
+    });
+  }, []);
+  return (
+    <div className="cursor" ref={cursorRef}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-6 h-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={4}
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </div>
+  );
+}
 
 function Rig() {
   const { camera, mouse } = useThree();
@@ -12,6 +52,44 @@ function Rig() {
       vec.set(mouse.x * 25, mouse.y * 10, camera.position.z),
       0.02
     )
+  );
+}
+interface PerspectiveButtonProps {
+  text: string;
+  turnY: number;
+}
+function PerspectiveButton({ text, turnY }: PerspectiveButtonProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="perspective__menu__button"
+      onMouseOver={() => setIsHovered((p) => true)}
+      onMouseLeave={() => setIsHovered((p) => false)}
+    >
+      <span>{text}</span>
+      {isHovered ? (
+        <>
+          <div
+            className="perspective__menu__button__holo"
+            style={{
+              transform: `rotateX(${0}deg) translateZ(15px)`,
+              top: `${turnY < 0 ? -Math.abs(turnY) : -Math.abs(turnY)}px`,
+              left: `${turnY < 0 ? turnY * 0.5 : turnY * 0.45}px`,
+            }}
+          ></div>
+          <div
+            className="perspective__menu__button__holo"
+            style={{
+              transform: `rotateX(${0}deg) translateZ(30px)`,
+              top: `${-Math.abs(turnY)}px`,
+              left: `${turnY}px`,
+              // border: `4px solid blue`,
+            }}
+          ></div>
+        </>
+      ) : null}
+    </div>
   );
 }
 
@@ -41,42 +119,18 @@ function Perspective() {
     <div
       ref={ref}
       className="project perspective"
-      onClick={() => console.log(turnYDegree())}
+      // onClick={() => console.log(turnYDegree())}
     >
+      <CustomCursor />
       <div
         className="perspective__menu"
         style={{
           transform: `rotateY(${turnYDegree()}deg) rotateX(${5}deg)`,
         }}
       >
-        <div className="perspective__menu__button">
-          <span>new game</span>
-          <div
-            className="perspective__menu__button__holo"
-            style={{
-              transform: `rotateX(${0}deg) translateZ(15px)`,
-              top: `${
-                turnYDegree() < 0
-                  ? -Math.abs(turnYDegree()) + 10
-                  : -Math.abs(turnYDegree()) + 10
-              }px`,
-              left: `${
-                turnYDegree() < 0 ? turnYDegree() * 0.5 : turnYDegree() * 0.45
-              }px`,
-            }}
-          ></div>
-          <div
-            className="perspective__menu__button__holo"
-            style={{
-              transform: `rotateX(${0}deg) translateZ(30px)`,
-              top: `${-Math.abs(turnYDegree())}px`,
-              left: `${turnYDegree()}px`,
-              // border: `4px solid blue`,
-            }}
-          ></div>
-        </div>
-        <div className="perspective__menu__button">settings</div>
-        <div className="perspective__menu__button">quit</div>
+        <PerspectiveButton text={"start"} turnY={turnYDegree()} />
+        <PerspectiveButton text={"settings"} turnY={turnYDegree()} />
+        <PerspectiveButton text={"quit"} turnY={turnYDegree()} />
       </div>
 
       <div className="perspective__canvas">
---
+++ pages/projects/wip/threeships.tsx
@@ -0,0 +1,58 @@
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Stars, OrbitControls, Environment } from "@react-three/drei";
+import Racer from "../../../components/threeships/racer";
+import Header from "../../../components/threeships/Header";
+
+// function Box() {
+//   const boxRef = useRef();
+//   useFrame(() => {
+//     boxRef.current.rotation.y += 0.001;
+//   });
+//   return (
+//     <mesh ref={boxRef}>
+//       <boxBufferGeometry attach="geometry" />
+//       <meshLambertMaterial attach="material" color="hotpink" />
+//     </mesh>
+//   );
+// }
+
+// function Ship() {
+//   const model = useLoader(GLTFLoader, "./models/craft_racer.glb");
+//   return (
+//     <Suspense fallback={null}>
+//       <primitive object={model.scene} />
+//     </Suspense>
+//   );
+// }
+
+function ThreeShips() {
+  return (
+    <div className="three">
+      <div className="ui">
+        <Header />
+        <div className="stats">
+          <div className="stats__content">
+            <h1>ACRacer</h1>
+          </div>
+        </div>
+      </div>
+      <div className="canvas">
+        <Canvas>
+          <Suspense fallback={null}>
+            <OrbitControls />
+            <Stars />
+            {/* <ambientLight intensity={3.9} /> */}
+            {/* <spotLight position={[3, 3, 3]} angle={0.5} />
+          <spotLight position={[-5, -5, -5]} angle={0.5} /> */}
+            <spotLight position={[0, 5, 0]} />
+            <Environment preset="sunset" />
+            <Racer />
+          </Suspense>
+        </Canvas>
+      </div>
+    </div>
+  );
+}
+
+export default ThreeShips;

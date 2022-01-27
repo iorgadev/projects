f11890780ee3e0f35c500d514e6a73b053b848c7
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-01-27 13:21:46 -0500

    wip threejs spaceship ui

---
+++ components/threeships/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="content">content</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/threeships/racer.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+export default function Model(props) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/models/craft_racer.glb");
+  // useFrame(() => {
+  //   group.current.rotation.y += 0.003;
+  // });
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group position={[0, 0, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh_craft_racer.geometry}
+          material={materials.metal}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh_craft_racer_1.geometry}
+          material={materials.metalDark}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh_craft_racer_2.geometry}
+          material={materials.dark}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Mesh_craft_racer_3.geometry}
+          material={materials.metalRed}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/craft_racer.glb");
